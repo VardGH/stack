6dbdc74734e941fdf1daf2129616896c04a181fb
Varduhi Ghukasyan <86279732+VardGH@users.noreply.github.com>
2023-05-17 22:01:40 +0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,74 +1,9 @@
 #include <iostream>
-#include "vector.hpp"
 #include "stack.hpp"
 
 namespace test
 {
-
-void test_vec()
-{
-    // vec
-    container::Vector<int> vec;
-    vec.push_back(1);
-    vec.push_back(2);
-    vec.push_back(3);
-    vec.push_back(4);
     
-    std::cout << "Print vec: ";
-    for (size_t i = 0; i < vec.size(); ++i) {
-        std::cout << vec[i] << " ";
-    }
-    std::cout << std::endl;
-
-    // vec1
-    container::Vector<int> vec1 = vec;
-    vec1.push_back(5);
-
-    std::cout << "Print vec1: ";
-    for (size_t i = 0; i < vec1.size(); ++i) {
-        std::cout << vec1[i] << " ";
-    }
-    std::cout << std::endl;
-
-    //vec2
-    const container::Vector<int> vec2 = vec1;
-    std::cout << "Print const vec2: ";
-    for (size_t i = 0; i < vec2.size(); ++i) {
-        std::cout << vec2[i] << " ";
-    }
-    std::cout << std::endl;
-
-    vec1.clear();
-    std::cout << "Print const vec2: ";
-    for (size_t i = 0; i < vec1.size(); ++i) {
-        std::cout << vec1[i] << " ";
-    }
-    std::cout << std::endl;
-    
-    // vec3
-    container::Vector<int> vec3;
-    vec3 = std::move(vec);
-
-    std::cout << "Print vec3: ";
-    for (size_t i = 0; i < vec3.size(); ++i) {
-        std::cout << vec3[i] << " ";
-    }
-    std::cout << std::endl;
-
-    std::cout << "vac2.back() " << vec2.back() << std::endl;
-    std::cout << "vac3.back() " << vec3.back() << std::endl;
-
-    try {
-        vec3.pop_back();
-        vec3.pop_back();
-        vec3.pop_back();
-        vec3.pop_back();
-        vec3.pop_back();
-    } catch (const std::exception& ex) {
-        std::cout << "Exception caught: " << ex.what() << std::endl;
-    }
-}
-
 void test_stack()
 {
     // st1
@@ -134,8 +69,7 @@ void test_stack()
     }
 }
 
-
-}
+} // namespace test
 
 int main() 
 {
